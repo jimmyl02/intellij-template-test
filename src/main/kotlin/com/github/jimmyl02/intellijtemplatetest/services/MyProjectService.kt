29f55863b8b89f2c8e9079fa3446af24a5426097
GitHub Action <action@github.com>
2025-09-15 01:44:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/jimmyl02/intellijtemplatetest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jimmyl02/intellijtemplatetest/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jimmyl02.intellijtemplatetest.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jimmyl02.intellijtemplatetest.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/jimmyl02/intellijtemplatetest/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/jimmyl02/intellijtemplatetest/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.jimmyl02.intellijtemplatetest.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
